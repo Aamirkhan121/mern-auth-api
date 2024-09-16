f477704bf6db2462876a9a58c072d0ccc4177d00
Aamirkhan121 <131248243+Aamirkhan121@users.noreply.github.com>
2024-09-16 13:51:18 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -29,12 +29,12 @@ const App = () => {
         <Route path='/register' element={<Register/>}/>
         <Route path='/login' element={<Login/>}/>
         <Route path='/logout' element={<Logout/>}/>
-        <Route path='/admin' element={<AdminLayout/>}>
+        <Route path='/admin' element={<AdminLayout/>/>}>
         <Route path="users/:id/edit" element={<AdminUserUpdated/>}/>
         <Route path='users' element={<AdminUsers/>}/>
         <Route path='contacts' element={<AdminContacts/>}/>
         {/* <Route path="contacts/:id/edit" element={<AdminContactUpdate/>}/> */}
-        </Route>
+       
 
         <Route path='*' element={<Error/>}/>
       </Routes>
